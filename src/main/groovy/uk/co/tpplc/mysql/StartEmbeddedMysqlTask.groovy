81cf4423c4ab488be942e50b29ac5de7d8997179
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2015-11-18 22:21:20 +0000

    adds functionality to start and stop tasks

--- src/main/groovy/uk/co/tpplc/mysql/StartEmbeddedMysqlTask.groovy
+++ src/main/groovy/uk/co/tpplc/mysql/StartEmbeddedMysqlTask.groovy
@@ -1,7 +1,34 @@
 package uk.co.tpplc.mysql
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+import static com.wix.mysql.EmbeddedMysql.anEmbeddedMysql
+import static com.wix.mysql.config.MysqldConfig.aMysqldConfig
+import static com.wix.mysql.distribution.Version.v5_6_22
 
 class StartEmbeddedMysqlTask extends DefaultTask {
+
+    StartEmbeddedMysqlTask() {
+        description 'starts an embedded mysql process'
+    }
+
+    @TaskAction
+    def run() {
+        def config = aMysqldConfig(v5_6_22)
+                .withPort(extension.port)
+                .withUser(extension.username, extension.password)
+                .build();
+
+        def mysql = anEmbeddedMysql(config)
+                .addSchema(extension.databaseName)
+                .start();
+
+        project.extensions.extraProperties.set('mysqlProcess', mysql)
+    }
+
+    def getExtension() {
+        return project.extensions.findByName('embeddedMysqlExtension') as EmbeddedMysqlExtension
+    }
 
 }
